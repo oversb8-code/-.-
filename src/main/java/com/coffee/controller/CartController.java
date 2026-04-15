5aca5734bb1995ab9730d61c06fbb17f31bbe35e
name <oversb8@gmail.com>
2026-04-15 16:22:09 +0900

    스프링 파일.

--- src/main/java/com/coffee/controller/CartController.java
+++ src/main/java/com/coffee/controller/CartController.java
@@ -3,6 +3,7 @@ package com.coffee.controller;
 import com.coffee.dto.CartItemDto;
 import com.coffee.dto.CartProductDto;
 import com.coffee.entity.Member;
+import com.coffee.service.CartProductService;
 import com.coffee.service.CartService;
 import com.coffee.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -42,4 +43,28 @@ public class CartController {
         }
         return ResponseEntity.ok(cartService.getCartItemsByMemberId(member.getId()));
     }
+
+    private final CartProductService cartProductService;
+
+@PatchMapping("/edit{cartProductId}")
+    public ResponseEntity<String> editCartProductQuantity(
+            @PathVariable Long cartProductId,
+            @RequestParam(required = false) Integer quantity,
+            @RequestParam(required = false) Long cartProductId){
+    System.out.println("카트 상품 아이디 : "+ cartProductId);
+    System.out.println("변경할 갯수 "+ quantity);
+        System.out.println("상품아이디 "+ productId);
+
+    String message = cartProductService.editCartProductQuantity(cartProductId,quantity,productId);
+    if(message.startsWith("오류")){
+        return ResponseEntity.badRequest().body(message);
+    }else {
+        return ResponseEntity.ok(message);
+    }
+
+    }
+    public ResponseEntity<String> deleteCartProduct(@PathVariable Long cartProductId){
+    return null;
+    }
+
 }
